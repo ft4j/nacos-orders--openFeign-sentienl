94db4dd0828febcdd70d30b7fcc0daf5e395dbd0
ft4j <342967138@qq,com>
2023-02-18 16:13:16 +0800

    第一次提交

---
+++ zzTest/src/main/java/com/tuling/springcloud/stock/ZzTestApplication.java
@@ -0,0 +1,11 @@
+package com.tuling.springcloud.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZzTestApplication.class, args);
+    }
+}
